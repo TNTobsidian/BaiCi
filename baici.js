24164366eca83a07488283bf52beb78561a1fd3d
TNTobsidian <65720409+TNTobsidian@users.noreply.github.com>
2021-10-06 22:19:23 +0800

    Create baici.js

---
+++ baici.js
@@ -0,0 +1,406 @@
+[
+        {
+            id: 1,//
+            answer: ["Lu BenWei","Xiao Zhan","Wang Yubo","Ma NiuBi"]//
+        },
+        {
+            id: 3,//
+            answer: "list"
+        },
+        {
+            id: 4,//
+            answer: "hill"
+        },
+        {
+            id: 5,//
+            answer: "light"
+        },
+        {
+            id: 6,//
+            answer: "journey"
+        },
+        {
+            id: 7,//第七题:你对另一半的身高要求具体是多少m？（数值）
+            answer: "lie"
+        },
+        {
+            id: 8,//Auto English Master
+            answer: "lake"
+        },
+        {
+            id: 9,//第七题:你认为大学期间情侣每天在一起多长时间合适？（X个小时）
+            answer: "lend"
+        },
+        {
+            id: 10,//Auto English Master
+            answer: "leave"
+        },
+        {
+            id: 11,//Auto English Master
+            answer: "least"
+        },
+        {
+            id: 12,//Auto English Master
+            answer: "king"
+        },
+        {
+            id: 13,//Auto English Master
+            answer: "leaves"
+        },
+        {
+            id: 14,//Auto English Master
+            answer: "leader"
+        },
+        {
+            id: 15,//Auto English Master
+            answer: "knock"
+        },
+        {
+            id: 16,//Auto English Master
+            answer: "laugh"
+        },
+        {
+            id: 17,//Auto English Master
+            answer: "last"
+        },
+        {
+            id: 18,//Auto English Master
+            answer: "language"
+        },
+        {
+            id: 19,//Auto English Master
+            answer: "level"
+        },
+        {
+            id: 20,//Auto English Master
+            answer: "knowledge"
+        },
+        {
+            id: 21,//Auto English Master
+            answer: "lead"
+        },
+        {
+            id: 22,//Auto English Master
+            answer: "knee"
+        },
+        {
+            id: 23,//Auto English Master
+            answer: "kitchen"
+        },
+        {
+            id: 24,//Auto English Master
+            answer: "learn"
+        },
+        {
+            id: 25,//Auto English Master
+            answer: "kind"
+        },
+        {
+            id: 26,//Auto English Master
+            answer: "key"
+        },
+        {
+            id: 27,//Auto English Master
+            answer: "jump"
+        },
+        {
+            id: 28,//Auto English Master
+            answer: "juice"
+        },
+        {
+            id: 29,//Auto English Master
+            answer: "judge"
+        },
+        {
+            id: 30,//Auto English Master
+            answer: "lift"
+        },
+        {
+            id: 31,//Auto English Master
+            answer: "joke"
+        },
+        {
+            id: 32,//Auto English Master
+            answer: "join"
+        },
+        {
+            id: 33,//Auto English Master
+            answer: "island"
+        },
+        {
+            id: 34,//Auto English Master
+            answer: "involve"
+        },
+        {
+            id: 35,//Auto English Master
+            answer: "invite"
+        },
+        {
+            id: 36,//Auto English Master
+            answer: "invention"
+        },
+        {
+            id: 37,//Auto English Master
+            answer: "invent"
+        },
+        {
+            id: 38,//Auto English Master
+            answer: "introduction"
+        },
+        {
+            id: 39,//Auto English Master
+            answer: "introduce"
+        },
+        {
+            id: 40,//Auto English Master
+            answer: "hole"
+        },
+        {
+            id: 41,//Auto English Master
+            answer: "interrput"
+        },
+        {
+            id: 42,//Auto English Master
+            answer: "international"
+        },
+        {
+            id: 43,//Auto English Master
+            answer: "interest"
+        },
+        {
+            id: 44,//Auto English Master
+            answer: "instrument"
+        },
+        {
+            id: 45,//Auto English Master
+            answer: "instruction"
+        },
+        {
+            id: 46,//Auto English Master
+            answer: "instead"
+        },
+        {
+            id: 47,//Auto English Master
+            answer: "insist"
+        },
+        {
+            id: 48,//Auto English Master
+            answer: "information"
+        },
+        {
+            id: 49,//Auto English Master
+            answer: "influence"
+        },
+        {
+            id: 50,//Auto English Master
+            answer: "infer"
+        },
+        {
+            id: 51,//Auto English Master
+            answer: "industry"
+        },
+        {
+            id: 52,//Auto English Master
+            answer: "large"
+        },
+        {
+            id: 53,//Auto English Master
+            answer: "increase"
+        },
+        {
+            id: 54,//Auto English Master
+            answer: "include"
+        },
+        {
+            id: 55,//Auto English Master
+            answer: "improve"
+        },
+        {
+            id: 56,//Auto English Master
+            answer: "impossible"
+        },
+        {
+            id: 57,//Auto English Master
+            answer: "important"
+        },
+        {
+            id: 58,//Auto English Master
+            answer: "library"
+        },
+        {
+            id: 59,//Auto English Master
+            answer: "immediately"
+        },
+        {
+            id: 60,//Auto English Master
+            answer: "imagine"
+        },
+        {
+            id: 61,//Auto English Master
+            answer: "hurt"
+        },
+        {
+            id: 62,//Auto English Master
+            answer: "humorous"
+        },
+        {
+            id: 63,//Auto English Master
+            answer: "hungry"
+        },
+        {
+            id: 64,//Auto English Master
+            answer: "chore"
+        },
+        {
+            id: 65,//Auto English Master
+            answer: "honor"
+        },
+        {
+            id: 66,//Auto English Master
+            answer: "interview"
+        },
+        {
+            id: 67,//Auto English Master
+            answer: "hold"
+        },
+        {
+            id: 68,//Auto English Master
+            answer: "hit"
+        },
+        {
+            id: 69,//Auto English Master
+            answer: "history"
+        },
+        {
+            id: 70,//Auto English Master
+            answer: "hero"
+        },
+        {
+            id: 71,//Auto English Master
+            answer: "litter"
+        },
+        {
+            id: 72,//Auto English Master
+            answer: "line"
+        },
+        {
+            id: 73,//Auto English Master
+            answer: "illness"
+        },
+        {
+            id: 74,//Auto English Master
+            answer: "letter"
+        },
+        {
+            id: 75,//Auto English Master
+            answer: "lazy"
+        },
+        {
+            id: 76,//Auto English Master
+            answer: "herself"
+        },
+        {
+            id: 77,//Auto English Master
+            answer: "high"
+        },
+        {
+            id: 78,//Auto English Master
+            answer: "lab"
+        },
+        {
+            id: 79,//Auto English Master
+            answer: "left"
+        },
+        {
+            id: 80,//Auto English Master
+            answer: "hide"
+        },
+        {
+            id: 81,//Auto English Master
+            answer: "limit"
+        },
+        {
+            id: 82,//Auto English Master
+            answer: "hobby"
+        },
+        {
+            id: 83,//Auto English Master
+            answer: "holiday"
+        },
+        {
+            id: 84,//Auto English Master
+            answer: "hometown"
+        },
+        {
+            id: 85,//Auto English Master
+            answer: "honest"
+        },
+        {
+            id: 86,//Auto English Master
+            answer: "horse"
+        },
+        {
+            id: 87,//Auto English Master
+            answer: "hospital"
+        },
+        {
+            id: 88,//Auto English Master
+            answer: "hotel"
+        },
+        {
+            id: 89,//Auto English Master
+            answer: "hour"
+        },
+        {
+            id: 90,//Auto English Master
+            answer: "huge"
+        },
+        {
+            id: 91,//Auto English Master
+            answer: "human"
+        },
+        {
+            id: 92,//Auto English Master
+            answer: "hurry"
+        },
+        {
+            id: 93,//Auto English Master
+            answer: "husband"
+        },
+        {
+            id: 94,//Auto English Master
+            answer: "ice-cream"
+        },
+        {
+            id: 95,//Auto English Master
+            answer: "idea"
+        },
+        {
+            id: 96,//Auto English Master
+            answer: "interesting"
+        },
+        {
+            id: 97,//Auto English Master
+            answer: "Internet"
+        },
+        {
+            id: 98,//Auto English Master
+            answer: "jacket"
+        },
+        {
+            id: 99,//Auto English Master
+            answer: "lesson"
+        },
+        {
+            id: 100,//Auto English Master
+            answer: "kick"
+        },
+        {
+            id: 101,//Auto English Master
+            answer: "keyboard"
+        },
+        {
+            id: 102,//Auto English Master
+            answer: "lay"
+        }
+    ]
